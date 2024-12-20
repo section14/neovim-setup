8e848eebc0e1b4268dd62f7851c926d3ee17976d
Anthony Henderson <anthony@enhancesoft.com>
2024-12-19 22:51:01 -0500

    add extra Go file types

--- lua/_lsp-golang.lua
+++ lua/_lsp-golang.lua
@@ -4,7 +4,7 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 lsp.gopls.setup{
 	cmd = { "gopls" },
-	filetypes = {"go", "gomod" },
+	filetypes = {"go", "gomod", "gowork", "gotmpl" },
 	root_dir = util.root_pattern("go.mod", ".git"),
   capabilities = capabilities 
 }
