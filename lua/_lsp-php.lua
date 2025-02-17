e7b87bf9b1b0d5921d938fc5e890222fd3e29735
Anthony Henderson <anthony@enhancesoft.com>
2025-02-17 11:04:30 -0500

    fix php config -- maybe?

--- lua/_lsp-php.lua
+++ lua/_lsp-php.lua
@@ -1,9 +1,11 @@
 local lsp = require('lspconfig')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 lsp.phpactor.setup {
     on_attach = on_attach,
     init_options = {
         ["language_server_phpstan.enabled"] = false,
         ["language_server_psalm.enabled"] = false,
-    }
+    },
+    capabilities = capabilities 
 }
